f255f54805a24999454cb02cccb2364b37eae98f
Volodya <volodymyr.shyrochuk@lametric.com>
2020-09-03 12:51:30 +0300

    Celan up app delegate

--- Blocks/AppDelegate.swift
+++ Blocks/AppDelegate.swift
@@ -7,15 +7,8 @@
 //
 
 import UIKit
-import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
 }
